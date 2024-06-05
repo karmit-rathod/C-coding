f7af21e2aa113785ef59e3135e57acb8eec5e782
jenishbabariya22 <karmitrathod990@gmail.com>
2024-06-05 13:11:07 +0530

    05/06/2024 github

---
+++ ch-2/2_2/USER_2.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int L ;
+	clrscr();
+	printf("Enter value of L:");
+	scanf("%d",&L);
+	printf("Area of rectangle is :\n");
+	printf("%i*%i=%i",L,L,L*L);
+	getch();
+
+
+
+}
---
+++ task/CAMARA1.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	 clrscr();
+	 printf("\tname : PRINCE\n");
+
+	 printf("\tage : 15\n");
+
+	 printf("\tschool : mauni international\n");
+
+
+	 getch();
+
+
+
+
+
+
+
+}
